7eaa50cb815ca5ab74b8186c065dc6d31394fe01
jofetta <jafarovatatiana@gmail.com>
2024-01-20 18:33:15 +0300

    feat: solve task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,3 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
+
+fs.mkdir(path.join(__dirname, '/files-copy'), { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+  }
+})
+
+fs.promises.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true })
+  .then(filenames => {
+    for (let filename of filenames) {
+      fs.unlink(path.join(__dirname, 'files-copy', filename.name), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      })
+    }
+  })
+
+fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
+  .then(filenames => {
+    for (let filename of filenames) {
+      let src = path.join(__dirname, 'files', filename.name);
+      let dest = path.join(__dirname, 'files-copy', filename.name);
+      fs.copyFile(src, dest, fs.constants.COPYFILE_FICLONE, (err) => {
+        if (err) {
+          console.log('Error Found:', err);
+        }
+      })
+    }
+  }) 

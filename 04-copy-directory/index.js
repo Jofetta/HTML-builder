acf646abe32975eaafc67d91215205018130c579
jofetta <jafarovatatiana@gmail.com>
2024-01-22 23:25:09 +0300

    fix: fix inconsistencies in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,33 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
+async function main() {
+  try {
+    await fs.promises.mkdir(path.join(__dirname, '/files-copy'), { recursive: true });
+    await fs.promises.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true })
+      .then(filenames => {
+        for (let filename of filenames) {
+          fs.promises.unlink(path.join(__dirname, 'files-copy', filename.name));
+        }
+      })
 
-fs.mkdir(path.join(__dirname, '/files-copy'), { recursive: true }, (err) => {
-  if (err) {
+    await fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
+      .then(filenames => {
+        for (let filename of filenames) {
+          let src = path.join(__dirname, 'files', filename.name);
+          let dest = path.join(__dirname, 'files-copy', filename.name);
+          fs.copyFile(src, dest, fs.constants.COPYFILE_FICLONE, (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+  })
+  }
+  catch (err) {
     console.log(err);
   }
-})
+}
 
-fs.promises.readdir(path.join(__dirname, 'files-copy'), { withFileTypes: true })
-  .then(filenames => {
-    for (let filename of filenames) {
-      fs.unlink(path.join(__dirname, 'files-copy', filename.name), (err) => {
-        if (err) {
-          console.log(err);
-        }
-      })
-    }
-  })
-
-fs.promises.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
-  .then(filenames => {
-    for (let filename of filenames) {
-      let src = path.join(__dirname, 'files', filename.name);
-      let dest = path.join(__dirname, 'files-copy', filename.name);
-      fs.copyFile(src, dest, fs.constants.COPYFILE_FICLONE, (err) => {
-        if (err) {
-          console.log('Error Found:', err);
-        }
-      })
-    }
-  }) 
+main();
